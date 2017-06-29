b8ab5ca58c25c00751fed592a0b4aaa0819a07c0
George Czabania <george@czabania.com>
2017-06-29 17:31:40 +1200

    Use 0 prefix

---
+++ 01_beep_boop.js
@@ -0,0 +1,4 @@
+// just to make sure everything is working, output the string "beep boop" with
+// a `console.log()`
+
+console.log('beep boop')
--- 1_beep_boop.js
+++
@@ -1,4 +0,0 @@
-// just to make sure everything is working, output the string "beep boop" with
-// a `console.log()`
-
-console.log('beep boop')
